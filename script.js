b7c6fd69ad55019edcfcb5cb11f2c4810ad849c8
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-04-03 12:57:34 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const btnEl = document.getElementById("btn");
-function joke(){
-    console.log("joke")
+const jokeEl = document.getElementById("content");
+const apiKey = "SjapSLcf+FWJN6yGs2mVQg==SEISupoOi6aSQKLe";
+const options = {
+    method: 'GET',
+    headers: { 'X-Api-Key': apiKey},
+};
+const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1"
+ async function joke(){
+    try { jokeEl.innerHTML="Loading..."
+    btnEl.dissabled = false;
+    btnEl.innerHTML= "Updating..."
+    const response = await fetch(apiUrl,options);
+    btnEl.dissabled = true;
+    btnEl.innerHTML= "Tell Me a Joke";
+    const data = await response.json();
+    jokeEl.innerHTML = data[0].joke;
+        
+    } catch (error) {
+
+      jokeEl.innerHTML = "An Error occured,Try Again Later"  
+      btnEl.dissabled = false;
+    btnEl.innerHTML= "Tell Me a Joke";
+    }
+   
 }
 btnEl.addEventListener("click",joke)
